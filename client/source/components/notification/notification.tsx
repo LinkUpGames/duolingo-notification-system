9082ad301368a4db9a9b0074f7c503e4cf38b036
marcosFIF <marcosc@fillitforward.com>
2025-08-06 08:04:56 -0400

    component change

---
+++ client/source/components/notification/notification.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * The notification gathered from the backend
+ */
+export type Notification = {};
+
+type Props = {};
+
+const Notification = ({}: Props) => {
+  return <></>;
+};
+
+export default Notification;

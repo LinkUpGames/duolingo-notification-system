3bb1303da0b9c344b72a06d4944a109513ac5cce
Anonymous <mjcevallos007@gmail.com>
2025-08-08 11:03:11 -0400

    Updated the transform function

--- scorer/src/transform.py
+++ scorer/src/transform.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def parse_decision(decision, probabilities, event):
     """
     Parse a decision and create a dictionary with the following values
@@ -7,25 +10,27 @@ def parse_decision(decision, probabilities, event):
     'notification_id': string
     'user_id': string
     'timestamp': milliseconds
-    'probabilities: [
-                    {
-                        'id': string,
-                        'user_id': string,
-                        'decision_id': string,
-                        'notification_id': string,
-                        'probability': number
-                    }
-    ]
-    'event': {
-            'decision_id': string,
-            'selected': boolean,
-            'timestamp': milliseconds
+    'probabilities: {
+            "notification_id": number
     }
+    'selected': boolean
+    'response_time': timestamp
     """
+    event_dict = {}
+    for prob in probabilities:
+        event_dict[prob["notification_id"]] = prob["probability"]
+
     decision_dict = {
         **decision,
-        "probabilities": probabilities,
-        "event": event,
+        "probabilities": event_dict,
+        "selected": event["selected"],
+        "response_time": event["timestamp"],
     }
 
     return decision_dict
+
+
+def parse_choices(logs):
+    """
+    Parse the choices where the notificaiton was selected and not select per round (decision log)
+    """

3cd4e2ec7a38c1cfded6ad0d334ed12739523b30
Anonymous <mjcevallos007@gmail.com>
2025-08-09 20:37:58 -0400

    refactoring the notification part of things

---
+++ scorer/extensions/algorithm/module/notification.c
@@ -0,0 +1,69 @@
+#include "notification.h"
+#include <stdlib.h>
+#include <string.h>
+
+Notification *create_notification(char const *id, float score,
+                                  float probability) {
+
+  Notification *notification = (Notification *)malloc(sizeof(Notification));
+
+  if (notification == NULL) {
+    return NULL;
+  }
+
+  notification->id = strdup(id);
+  notification->probability = probability;
+  notification->score = score;
+
+  // Setup the compute sums for later
+  notification->m_plus = 0;
+  notification->m_plus_count = 0;
+  notification->m_plus_sum = 0;
+
+  notification->m_minus = 1; // Avoid division by zero later
+  notification->m_minus_count = 0;
+  notification->m_minus_sum = 0;
+
+  return notification;
+}
+
+void free_notification(Notification *notification) {
+  if (notification->id != NULL) {
+    free(notification->id);
+    notification->id = NULL;
+  }
+
+  free(notification);
+}
+
+NotificationArray *create_notification_list(long length) {
+
+  Notification **notifications =
+      (Notification **)calloc(length, sizeof(Notification *));
+
+  if (notifications == NULL) {
+    return NULL;
+  }
+
+  NotificationArray *array =
+      (NotificationArray *)malloc(sizeof(NotificationArray));
+  if (array == NULL) {
+    return NULL;
+  }
+  array->length = length;
+  array->array = notifications;
+  array->current_id = 0;
+
+  return array;
+}
+
+void free_notification_list(NotificationArray *notifications) {
+  if (notifications != NULL) {
+    for (size_t i = 0; i < notifications->length; i++) {
+      free_notification(notifications->array[i]);
+      notifications->array[i] = NULL;
+    }
+
+    free(notifications);
+  }
+}
--- scorer/extensions/algorithm/module/notification.h
+++ scorer/extensions/algorithm/module/notification.h
@@ -1,4 +1,73 @@
 #ifndef NOTIFICATION_H
 #define NOTIFICATION_H
 
+#include <Python.h>
+
+// Notification The notification structure used for
+typedef struct Notification {
+  // Id of the notification
+  char *id;
+
+  // The score for the notification
+  float score;
+
+  // The probability that the notification was selected
+  float probability;
+
+  // rewarded weight sum when selected
+  float m_plus_sum;
+
+  // weight sum when selcted
+  float m_plus_count;
+
+  // reward weight sum when not selected
+  float m_minus_sum;
+
+  // weight sum when not selected
+  float m_minus_count;
+
+  // The average weight when the notification is selected
+  float m_plus;
+
+  // The average weight when the notification was not selected;
+  float m_minus;
+
+} Notification;
+
+// The notification array that holds the information for a notification
+typedef struct NotificationArray {
+  Notification **array;
+
+  long length;
+
+  size_t current_id;
+} NotificationArray;
+
+/**
+ * Create a notification struct with the following values
+ * @param id The notification id
+ * @param score The score for the notification
+ * @param probability The probability that this notification is selected
+ */
+Notification *create_notification(char const *id, float score,
+                                  float probability);
+
+/**
+ * Create a notificaiton list of length
+ * @param length The length of the array
+ */
+NotificationArray *create_notification_list(long length);
+
+/**
+ * Free the notification list
+ * @param length The length of the array
+ */
+void free_notification_list(NotificationArray *array);
+
+/**
+ * Free the memory for the notification struct that was allocated
+ * @param notification The notification to free
+ */
+void free_notification(Notification *notification);
+
 #endif
